cfb31be2655a99a120010c1eef459a72d71fa036
agusdwis <agusdwi@alterra.id>
2020-06-16 17:16:42 +0700

    update

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,15 +3,18 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
 import NavigationBar from "../components/NavigationBar";
+import Link from "../utils/Link";
 
 export default function About() {
     return (
         <React.Fragment>
             <NavigationBar/>
             <Container maxWidth="sm">
-                <Box my={4}>
-                    ABOUT
-                </Box>
+                <Link href={'/'}>
+                    <Box my={4}>
+                        <h4 style={{color: 'red'}}>ABOUT</h4>
+                    </Box>
+                </Link>
             </Container>
         </React.Fragment>
 
