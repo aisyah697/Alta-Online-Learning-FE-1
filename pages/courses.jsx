b68f8d17726051c745864e740e8962cd947add7d
agusdwis <agusdwi@alterra.id>
2020-06-20 18:02:08 +0700

    update

--- pages/courses.jsx
+++ pages/courses.jsx
@@ -14,10 +14,15 @@ import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import {Done} from "@material-ui/icons";
-import RemoveIcon from '@material-ui/icons/Remove';
+import Button from "@material-ui/core/Button";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -82,14 +87,36 @@ const useStyles = makeStyles((theme) => ({
     },
     box3: {
         paddingTop: theme.spacing(2),
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     rootList: {
         width: '100%',
         maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
-    }
+    },
+    button: {
+        backgroundColor: theme.palette.secondary.main,
+        borderColor: theme.palette.secondary.main,
+        color: theme.palette.common.white,
+        padding: '5px 20px',
+        textTransform: 'none',
+        borderRadius: theme.spacing(1),
+        minWidth: theme.spacing(12),
+        '&:hover' : {
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.secondary.main,
+            textDecoration: 'none',
+            borderColor: theme.palette.secondary.main,
+        },
+    },
+    table: {
+        minWidth: 700,
+    },
 }))
 
+// Tab
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
     const classes = useStyles()
@@ -132,7 +159,6 @@ const StyledTab = withStyles((theme) => ({
         fontSize: `calc(1rem+1vw)`,
         '&:focus': {
             opacity: 1,
-            fontWeight: 600,
         },
     },
     wrapper: {
@@ -143,43 +169,7 @@ const StyledTab = withStyles((theme) => ({
     }
 }))((props) => <Tab disableRipple {...props} />);
 
-function CircularProgressWithLabel(props) {
-    return (
-        <Box position="relative" display="inline-flex">
-            <CircularProgress size={100} color={'secondary'} variant="static" {...props} />
-            <Box
-                top={0}
-                left={0}
-                bottom={0}
-                right={0}
-                position="absolute"
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-            >
-                <Typography variant="caption" component="div" color="secondary">{`${Math.round(
-                    props.value,
-                )}%`}</Typography>
-            </Box>
-        </Box>
-    );
-}
-
-CircularProgressWithLabel.propTypes = {
-    value: PropTypes.number.isRequired,
-};
-
-const ListItemBase = ({subject, done}) => {
-    return (
-        <React.Fragment>
-            <ListItem>
-                <ListItemText primary={subject} />
-                {done ? <Done /> : null}
-            </ListItem>
-        </React.Fragment>
-    )
-}
-
+// Current Course
 const CurrentCourse = () => {
     const classes = useStyles();
     
@@ -194,6 +184,7 @@ const CurrentCourse = () => {
                     Active Course
                 </Typography>
             </div>
+            <br/>
             <div className={classes.currentContent}>
                 <Paper elevation={0} className={classes.paper}>
                     <div className={classes.paperContent}>
@@ -221,17 +212,124 @@ const CurrentCourse = () => {
                                         <ListItemBase subject={'Basic of Python 3'}/>
                                     </List>
                                 </Grid>
-                                <Grid item xs={12} lg={3} className={classes.box3}>
-                                    CC
+                                <Grid item xs={12} lg={6} className={classes.box3}>
+                                    <Button className={classes.button} variant={'outlined'}>
+                                        Go to course
+                                    </Button>
                                 </Grid>
                             </Grid>
                         </div>
                     </div>
                 </Paper>
+                <br/>
+                <Paper elevation={0} className={classes.paper}>
+                    <CustomizedTables/>
+                </Paper>
             </div>
         </React.Fragment>
     )
 }
+
+function CircularProgressWithLabel(props) {
+    return (
+        <Box position="relative" display="inline-flex">
+            <CircularProgress size={100} color={'secondary'} variant="static" {...props} />
+            <Box
+                top={0}
+                left={0}
+                bottom={0}
+                right={0}
+                position="absolute"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Typography variant="caption" component="div" color="secondary">
+                    {`${Math.round(props.value)}%`}
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+CircularProgressWithLabel.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+const ListItemBase = ({subject, done}) => {
+    return (
+        <React.Fragment>
+            <ListItem>
+                <ListItemText primary={subject} />
+                {done ? <Done /> : null}
+            </ListItem>
+        </React.Fragment>
+    )
+}
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.secondary.secondary,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+function createData(name, quiz1, quiz2, quiz3) {
+    return { name, quiz1, quiz2, quiz3 };
+}
+
+const rows = [
+    createData('What is Python', 'Bad', 'Bad', 'Good'),
+    createData('Algorithm in Python', 'Good', 'Good', 'Good',),
+    createData('Basic of Python 1', 'Excellent', 'Excellent', 'Excellent'),
+    createData('Basic of Python 2', 'Excellent', 'Excellent', 'Excellent'),
+    createData('Basic of Python 3', 'Excellent', 'Excellent', 'Excellent'),
+];
+
+function CustomizedTables() {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper} elevation={1}>
+            <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Subject</StyledTableCell>
+                        <StyledTableCell align="center">Quiz 1</StyledTableCell>
+                        <StyledTableCell align="center">Quiz 2</StyledTableCell>
+                        <StyledTableCell align="center">Quiz 3</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.name}>
+                            <StyledTableCell component="th" scope="row">
+                                {row.name}
+                            </StyledTableCell>
+                            <StyledTableCell align="center">{row.quiz1}</StyledTableCell>
+                            <StyledTableCell align="center">{row.quiz2}</StyledTableCell>
+                            <StyledTableCell align="center">{row.quiz3}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+// Past Course
+// Certificate
 
 export default function CoursePage() {
     const classes = useStyles();
