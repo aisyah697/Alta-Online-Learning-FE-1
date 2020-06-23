ff939804f2cbd9f51ffc0b378f2dd9a87998a6de
aisyah697 <aisyah@alterra.id>
2020-06-23 14:05:40 +0700

    add quiz content unfinish

--- components/QuizContent.jsx
+++ components/QuizContent.jsx
@@ -5,12 +5,16 @@ const useStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette.secondary.secondary,
   },
+  content: {
+    flexGrow: 1,
+    paddingLeft: 0,
+    paddingRight: theme.spacing(5),
+    paddingTop: 0,
+  },
 }));
 
 export default function QuizContent(props) {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return(
-        
-    )
+  return <main></main>;
 }
