eaed9264e15b8df84d93f753dc8f284b0237c4b4
aisyah697 <aisyah@alterra.id>
2020-07-04 19:43:17 +0700

    fixing home mentee route

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,69 @@
 import React from "react";
 import Head from "next/head";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
-const NavigationBar = dynamic(() => import('../components/NavigationBar'))
-const HomePhaseMenu = dynamic(() => import('../components/home/HomePhaseMenu'))
-const HomeTestimony = dynamic(() => import('../components/home/HomeTestimony'))
-const HomeBanner = dynamic(() => import('../components/home/HomeBanner'))
-const FrequentQuestion = dynamic(() => import('../components/home/HomeFAQ'))
-const SubFooter = dynamic(() => import('../components/SubFooter'))
-const Footer = dynamic(() => import('../components/FooterBar'))
+const NavigationBar = dynamic(() => import("../components/NavigationBar"));
+const HomePhaseMenu = dynamic(() => import("../components/home/HomePhaseMenu"));
+const HomeTestimony = dynamic(() => import("../components/home/HomeTestimony"));
+const HomeBanner = dynamic(() => import("../components/home/HomeBanner"));
+const FrequentQuestion = dynamic(() => import("../components/home/HomeFAQ"));
+const SubFooter = dynamic(() => import("../components/SubFooter"));
+const Footer = dynamic(() => import("../components/FooterBar"));
 
 const Home = () => {
-    const [cookies] = useCookies()
-    
-    const [phase, setPhase] = React.useState()
-    
-    React.useEffect(() => {
-        const url = process.env.NEXT_PUBLIC_BASE_URL + "/historyphase/mentee";
-        const auth = cookies.token_mentee
-        const fetchData = async function () {
-            try {
-                const response = await axios.get(url, {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: "Bearer " + auth,
-                    },
-                });
-                if (response.status === 200) {
-                    setPhase(response.data);
-                }
-            } catch (error) {
-                throw error;
-            }
-        };
-        fetchData();
-    }, []);
+  const [cookies] = useCookies();
 
-    return (
-        <div>
-            <Head>
-                <title>Home | Alta Online Learning</title>
-            </Head>
-            <main>
-                <NavigationBar/>
-                <HomeBanner phase={phase}/>
-                {phase != "undefined" && phase != null && phase.length != null && phase.length > 0 ?
-                    <HomePhaseMenu phase={phase}/> : <></>
-                }
-                <HomeTestimony/>
-                <FrequentQuestion/>
-                <SubFooter phase={phase}/>
-                <Footer/>
-            </main>
-        </div>
-    )
-}
+  const [phase, setPhase] = React.useState();
+
+  React.useEffect(() => {
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/historyphase/mentee";
+    const auth = cookies.token_mentee;
+    const fetchData = async function () {
+      try {
+        const response = await axios.get(url, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + auth,
+          },
+        });
+        if (response.status === 200) {
+          setPhase(response.data);
+        }
+      } catch (error) {
+        throw error;
+      }
+    };
+
+    if (cookies.token_mentee) {
+      fetchData();
+    }
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>Home | Alta Online Learning</title>
+      </Head>
+      <main>
+        <NavigationBar />
+        <HomeBanner phase={phase} />
+        {phase != "undefined" &&
+        phase != null &&
+        phase.length != null &&
+        phase.length > 0 ? (
+          <HomePhaseMenu phase={phase} />
+        ) : (
+          <></>
+        )}
+        <HomeTestimony />
+        <FrequentQuestion />
+        <SubFooter phase={phase} />
+        <Footer />
+      </main>
+    </div>
+  );
+};
 
 export default Home;
